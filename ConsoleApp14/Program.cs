e7afdee18b2c3b76df1c68d1bc17b5504ea3bc5f
William Oviedo <oviedo7898@gmail.com>
2024-06-18 14:50:04 -0500

    Agregar archivos de proyecto.

---
+++ ConsoleApp14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class program
+{
+    static void Main ()
+    {
+        Console.WriteLine("Cuantos numeros desea ingresar->");
+        int cantidad = int.Parse (Console.ReadLine());
+
+        int[] numeros = new int[cantidad];
+
+        Console.WriteLine("ingrese los numeros->");
+        for (int i = 0; i < cantidad; i++)
+        {
+            numeros[i] = int.Parse (Console.ReadLine());
+        }
+        Array.Sort (numeros);
+
+        Console.WriteLine("numeros ordenados:");
+        foreach (int numero in numeros)
+        {
+            Console.WriteLine(numero);
+        }
+    }
+}
